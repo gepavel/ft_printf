e2eee8ad0801b53b88df149222e0037108cba740
gepavel <150628332+gepavel@users.noreply.github.com>
2024-03-01 14:41:36 +0100

    c, s, p, xX. FIXX

--- ft_printf_p.c
+++ ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: gepavel <gepavel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:53:54 by gepavel           #+#    #+#             */
-/*   Updated: 2024/02/20 14:39:27 by gepavel          ###   ########.fr       */
+/*   Updated: 2024/03/01 13:39:53 by gepavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,17 @@ static int	ft_print_adress(unsigned long int n, int count)
 	if (n >= 16)
 		count += ft_print_adress(n / 16, count);
 	if (nb < 10)
-		count += ft_printf_c(n % 16 + '0');
+	{
+		if (ft_printf_c(n % 16 + '0') != 1)
+			return (-1);
+		count++;
+	}
 	if (nb > 9)
-		count += ft_printf_c((n % 16 - 10) + 'a');
+	{
+		if (ft_printf_c(n % 16 - 10 + 'a') != 1)
+			return (-1);
+		count++;
+	}
 	return (count);
 }
 
@@ -33,6 +41,8 @@ int	ft_printf_p(unsigned long int n)
 	if (n == 0)
 		return (ft_printf_s("0x0"));
 	count = ft_printf_s("0x");
+	if (count == -1)
+		return (-1);
 	count += ft_print_adress(n, 0);
 	return (count);
 }
--- ft_printf_xX.c
+++ ft_printf_xX.c
@@ -6,37 +6,35 @@
 /*   By: gepavel <gepavel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:55:46 by gepavel           #+#    #+#             */
-/*   Updated: 2024/02/20 14:45:43 by gepavel          ###   ########.fr       */
+/*   Updated: 2024/03/01 14:40:08 by gepavel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_strcpy(char *dst, const char *src)
-{
-	size_t		i;
-
-	i = 0;
-	while (src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-}
-
 int	ft_printf_x(unsigned int nb, char base_set)
 {
-	int		count;
-	char	base[16];
+	int				count;
+	char			*base;
+	unsigned int	x;
 
 	count = 0;
+	x = nb % 16;
 	if (base_set == 'x')
-		ft_strcpy(base, "0123456789abcdef");
-	else if (base_set == 'X')
-		ft_strcpy(base, "0123456789ABCDEF");
+		base = "0123456789abcdef";
+	if (base_set == 'X')
+		base = "0123456789ABCDEF";
 	if (nb > 15)
-		count += ft_printf_x(nb / 16, base_set);
-	count += ft_printf_c(base[nb % 16]);
+	{
+		count = ft_printf_x(nb / 16, base_set);
+		if (count == -1)
+			return (-1);
+	}
+	if (x < 16)
+	{
+		if (ft_printf_c(base[nb % 16]) != 1)
+			return (-1);
+		count++;
+	}
 	return (count);
 }
